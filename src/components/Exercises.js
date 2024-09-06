4f981af37a31a8c04fbc7f9e36ac34f6a2316dbe
Rohit Saseendran <saseendranrohit3002@gmail.com>
2024-09-06 11:31:03 +0530

    search and scroll

---
+++ src/components/Exercises.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { useEffect, useState } from 'react'
+import { Pagination } from '@mui/material/Pagination'
+import { Box, Stack, Typography } from '@mui/material'
+import { exerciseOptions, fetchData } from '../utils/fetchData'
+
+const Exercises = () => {
+  return (
+    <div>Exercises</div>
+  )
+}
+
+export default Exercises
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {Box} from '@mui/material'
+import Exercises from '../components/Exercises'
+import SearchExercises from '../components/SearchExercises'
+import HeroBanner from '../components/HeroBanner'
 
 const Home = () => {
+  const[bodyPart, setBodyPart] = useState('all')
+  const[exercises, setExercises] = useState([])
+
   return (
-    <div>Home</div>
+    <Box>
+      <HeroBanner />
+      <SearchExercises 
+      setExercises={setExercises}
+      bodyPart={bodyPart}
+      setBodyPart={setBodyPart}
+      />
+      <Exercises 
+      exercises={exercises}
+      setExercises={setExercises}
+      bodyPart={bodyPart}
+      />
+    </Box>
   )
 }
 
